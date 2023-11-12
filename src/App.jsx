9a3aacf2c744ae6966d50a8b76e2d07285306e42
bserab <chmt22074@g.nihon-u.ac.jp>
2023-11-12 22:09:06 +0900

    画像の明度変更

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+export default function App() {
+    const images = [
+        { src: "images/pic1.jpg", alt: "Closeup of a human eye" },
+        { src: "images/pic2.jpg", alt: "Rock that looks like a wave" },
+        { src: "images/pic3.jpg", alt: "Purple and white pansies" },
+        { src: "images/pic4.jpg", alt: "Section of wall from a pharoah's tomb" },
+        { src: "images/pic5.jpg", alt: "Large moth on a leaf" }
+    ];
+
+    const [currentImage, setCurrentImage] = useState(images[0]);
+    const [bright, setBright] = useState(true);
+
+    const displayImage = (image) => {
+        setCurrentImage(image);
+    };
+
+    const changeBright=()=>{
+        setBright(!bright);
+    }
+
+    const checkLight = bright ? 'light' : 'dark'
+    const buttonText = bright ? 'darken' : 'lighten'
+    return (
+        <>
+            <h1>Image gallery example</h1>
+            <div className="full-img">
+                <img
+                    className="displayed-img"
+                    src={currentImage.src}
+                    alt={currentImage.alt}
+                />
+                <div className={checkLight}></div>
+                <button onClick={changeBright}>{buttonText}</button>
+            </div>
+            <div className="thumb-bar">
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        src={image.src}
+                        alt={image.alt}
+                        onClick={() => displayImage(image)}
+                    />
+                ))}
+            </div>
+        </>
+    );
+}
